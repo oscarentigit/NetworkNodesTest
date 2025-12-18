0ca345b48ca7c3d9dbeb391a9f8cad68c00fda78
oscarentigit <oscar.saborido@estudiant.enti.cat>
2025-12-18 16:49:01 +0100

    Escanear y buscarporIP

--- NetworkDemo/src/Red.java
+++ NetworkDemo/src/Red.java
@@ -35,12 +35,43 @@
 
         void escanearDesde(Nodo origen) {
             // TODO: BFS desde origen - https://www.geeksforgeeks.org/dsa/breadth-first-search-or-bfs-for-a-graph/
+            static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+            int V = adj.size();
+            boolean[] visited = new boolean[V];
+            ArrayList<Integer> res = new ArrayList<>();
+            
+            int src = 0;
+            Queue<Integer> q = new LinkedList<>();
+            visited[src] = true;
+            q.add(src);
+
+            while (!q.isEmpty()) {
+                int curr = q.poll();
+                res.add(curr);
+
+                // visit all the unvisited
+                // neighbours of current node
+                for (int x : adj.get(curr)) {
+                    if (!visited[x]) {
+                        visited[x] = true;
+                        q.add(x);
+                    }
+                }
+            }
+        
+        return res;
+    }
             // Reglas:
             // - si un nodo visitado es vulnerable => comprometido=true
             // - si un nodo visitado es firewall => NO se propaga a sus vecinos
         }
 
         Nodo buscarPorIP(String ip) {
+            for (Nodo n : nodos) {
+                if (n.getIp().equals(ip)) {
+                    return n;
+                }
+            }
             // Buscar por IP (iterar nodos)
             return null;
         }
